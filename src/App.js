7eb6557e96df77028d1bce0c12c084b9e9bf85e4
Jory Oberlies <joryoberlies@gmail.com>
2021-04-28 05:53:46 -0700

    2.6

--- src/App.js
+++ src/App.js
@@ -4,19 +4,35 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleAdd}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <li style={{ listStyleType: "none" }} key={person.name}>
+          {person.name}
+        </li>
+      ))}
     </div>
   );
 };
